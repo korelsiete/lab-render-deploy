4949dd7bc34e8fb5966a7e11ea4f550af1c43eea
Stefano Flores <stefanoypf@gmail.com>
2024-07-23 22:41:01 -0500

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const Note = require("./models/note");
+const Note = require("./models/note.js");
 const cors = require("cors");
 const app = express();
 
@@ -24,10 +24,6 @@ app.use(cors());
 app.use(express.static("dist"));
 app.use(express.json());
 app.use(requestLogger);
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
 
 app.get("/api/notes", (req, res) => {
   Note.find({}).then((notes) => {
